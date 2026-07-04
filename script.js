1d888b079002cbdfc3c2f3b59427f73dd72f26fe
saksham12-tech <sakshamy341@gmail.com>
2026-07-04 11:34:35 +0530

    add some feature

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let cart=[];
+
 const products=[
     {productId:"101", productName:"Samosa" ,price:10},
     {productId:"102", productName:"paneer Tika" ,price:150},
@@ -9,6 +11,7 @@ const productfilterinput=document.getElementById("product-filter");
 const productpriceinput=document.getElementById("product-price");
 const productqtyinput=document.getElementById("product-qty");
 const addtocart=document.getElementById("add-to-cart");
+const tablebody=document.getElementById("cart-items-body");
 
 function getData (value){
     const product=products.filter(data=>data.productId===value);
@@ -24,8 +27,63 @@ function getData (value){
     }
 }
 
+function addNewRow(Desc,Price,Qty){
+    const newRow=tablebody.insertRow(); // create automatically Row and append also.
+
+    const uniqueId=Date.now().toString();  //give id according to time;
+    newRow.setAttribute("data-id",uniqueId); 
+
+    const cell1=newRow.insertCell(0);
+    const cell2=newRow.insertCell(1);
+    const cell3=newRow.insertCell(2);
+    const cell4=newRow.insertCell(3);
+    const cell5=newRow.insertCell(4);
+
+    cell1.textContent=Desc;
+    cell2.textContent=Price;
+    cell3.textContent=Qty;
+    cell4.textContent=(Price*Qty).toFixed(2);
+    cell5.innerHTML=`<button class="delete-btn">&#10060;</button>`;
+
+    const item={
+        id:uniqueId,
+        productName:Desc,
+        productPrice:Price,
+        productQty:Qty,
+        total:(Price*Qty).toFixed(2),
+    }
+    cart.push(item);  //putting all items in cart if we referesh it should not delete
+    console.log(cart);
+
+}
+
 productfilterinput.addEventListener("keydown", (event)=>{
     if(event.key==="Enter" && event.target.value!==""){
         getData(event.target.value);
     }
+})
+
+addtocart.addEventListener("click",(e)=>{
+    if(productfilterinput.value==="" || productpriceinput.value===""||productqtyinput.value===""){
+        window.alert("Please fill all the fields");
+        return;
+    }
+    addNewRow(productfilterinput.value,productpriceinput.value,productqtyinput.value);
+    productfilterinput.value="";
+    productpriceinput.value="";
+    productqtyinput.value=1;
+    productfilterinput.focus();
+})
+
+tablebody.addEventListener("click",(e)=>{
+    const button=e.target.closest(".delete-btn");
+
+    if(button){
+        const row=button.closest("tr");
+        const rowid=row.getAttribute("data-id");
+        cart=cart.filter(item=>item.id!=rowid);
+        console.log(cart);
+        row.remove();
+    }
+
 })
